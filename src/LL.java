9c1ca723ef9a98844fe01cef4d4b2542529be0a3
Al - Amin <2111521017_al-amin@student.unand.ac.id>
2022-12-07 21:16:58 +0700

    udah

--- src/LL.java
+++ src/LL.java
@@ -20,88 +20,91 @@ public class LL {
         list.addAll(ll);
         System.out.println("isi linkedlist baru setelah ditambah linkedlist lama :" + list);
         System.out.println("-----------------------------------------------------------------");
-       
+
         System.out.println("Hasil method element() : " + list.element());
         System.out.println("Hasil linkedlist setelah dipakai method element() : " + list);
-        //hasilnya tidak akan ada perubahan dari linkedlist sebelumnya
+        // hasilnya tidak akan ada perubahan dari linkedlist sebelumnya
         System.out.println("---------------------------------------------------------------");
-        
-        //jenis jenis method get
+
+        // jenis jenis method get
         System.out.println("Hasil dari method get(int index) : " + list.get(4));
-        System.out.println("Hasil dari method getFirst() : " +list.getFirst());
+        System.out.println("Hasil dari method getFirst() : " + list.getFirst());
         System.out.println("Hasil dari method getFirst() : " + list.getLast());
 
-        //method indexof
+        // method indexof
         System.out.println("Hasil dari method indexof : " + list.indexOf("C"));
+        System.out.println("---------------------------------------------------");
 
-        //method offer
-        System.out.println("Linkedlist sebelum dioffer : "+list);
+        // method offer
+        System.out.println("Linkedlist sebelum dioffer : " + list);
         list.offer("Portugal");
         System.out.println("Hasil method offer : " + list);
-
         list.offerFirst("Brazil");
         System.out.println("hasil method offerFirst : " + list);
-
         list.offerLast("Spanyol");
-        System.out.println(list);
+        System.out.println("Hasil method offerLast : " + list);
+        System.out.println("-----------------------------------------");
 
-        
-        System.out.println(list.peek());
-        System.out.println(list.peekFirst());
-        System.out.println(list.peekLast());
+        // method peek
+        System.out.println("Hasil dari method peek() : " + list.peek());
+        System.out.println("Hasil dari method peekFirst() : " + list.peekFirst());
+        System.out.println("Hasil dari method peekLast() : " + list.peekLast());
+        System.out.println("----------------------------------------------------------");
 
+        // method poll
         list.poll();
-        System.out.println(list);
+        System.out.println("Linkedlist setelah di poll : " + list);
 
         list.pollFirst();
-        System.out.println(list);
-        
+        System.out.println("Linkedlist setelah di pollFirst : " + list);
+
         list.pollLast();
-        System.out.println(list);
+        System.out.println("Linkedlist setelah di pollLast : " + list);
 
+        // method pop
         list.pop();
-        System.out.println(list);
+        System.out.println("Linkedlist setelah di pop : " + list);
 
+        // method push
         list.push("Brazil");
-        System.out.println(list);
-
+        System.out.println("Linkedlist setelah di push : " + list);
+        System.out.println("----------------------------------------");
+        
         list.remove(1);
-        System.out.println(list);
-
+        System.out.println("Linkedlist setelah di remove(index) : " + list);
+        
         list.remove("B");
-        System.out.println(list);
-
+        System.out.println("Linkedlist setelah di remove(object) : " + list);
+        
         list.removeFirst();
-        System.out.println(list);
-
+        System.out.println("Linkedlist setelah di removeFirst() : " + list);
+        
         list.removeLast();
-        System.out.println(list);
-
+        System.out.println("Linkedlist setelah di removeLast() : " + list);
+        System.out.println("-----------------------------------------------");
+        
+        //method remove occurrence
         list.addFirst("Brazil");
         list.addFirst("Brazil");
         list.addLast("Brazil");
-        System.out.println("LL sebelum :"+list);
+        System.out.println("LL sebelum :" + list);
+        
 
         list.removeFirstOccurrence("Brazil");
-        System.out.println(list);
-
+        System.out.println("Hasil dari removeFirstOccurrence : " + list);
+        
         list.removeLastOccurrence("Brazil");
-        System.out.println(list);
-
-        list.set(2,"Belanda");
-        System.out.println(list + "Dengan ukuran LinkedList ="+list.size());
-
-
+        System.out.println("Hasil dari removeLastOccurrence : " + list);
+        System.out.println("----------------------------------------");
         
-
+        //method set
+        list.set(2, "Belanda");
+        System.out.println("Hasil dari method set : " +list + "Dengan ukuran LinkedList =" + list.size());
+        System.out.println("----------------------------------------");
         
-
-
-
-
+        //method clear
         list.clear();
-        System.out.println("isi linkedlist baru setelah diclear : "+list);
+        System.out.println("isi linkedlist baru setelah diclear : " + list);
 
-        
     }
 }
